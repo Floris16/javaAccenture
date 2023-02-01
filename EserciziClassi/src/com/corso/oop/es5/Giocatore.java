0350ea5e60973bc76720c7e0737f9dcb75d075f6
Floris16 <gabry.floris00@gmail.com>
2023-02-01 15:15:45 +0100

    create info sul giocatore

--- EserciziClassi/src/com/corso/oop/es5/Giocatore.java
+++ EserciziClassi/src/com/corso/oop/es5/Giocatore.java
@@ -1,5 +1,20 @@
 package com.corso.oop.es5;
 
 public class Giocatore {
+	String nome;
+	Carta cartainmano;
+	int punteggio;
+	
+	public Giocatore() {
+	}
+	
+	public Giocatore(String nome) {
+		this.nome=nome;
+	}
 
+	public int getPunteggio() {
+		return punteggio;
+	}
+	
+	
 }
