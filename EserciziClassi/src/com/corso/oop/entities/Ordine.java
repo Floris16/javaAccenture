79c83f6a94a0a8e0270f940b067b92e029e2a293
Floris16 <gabry.floris00@gmail.com>
2023-02-02 18:27:56 +0100

    .

--- EserciziClassi/src/com/corso/oop/entities/Ordine.java
+++ EserciziClassi/src/com/corso/oop/entities/Ordine.java
@@ -1,5 +1,29 @@
 package com.corso.oop.entities;
 
-public class Ordine {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Ordine {
+	Date dataOrdine;
+	int numero;
+	Cliente cliente;
+	ArrayList<Articolo> listaArticoli;
+	
+	public Ordine() {
+		this.dataOrdine=new Date();
+		this.numero=0;
+		this.cliente=new Cliente();
+		this.listaArticoli=new ArrayList<Articolo>();
+	}
+
+	public Ordine(Date dataOrdine, int numero, Cliente cliente, ArrayList<Articolo> listaArticoli) {
+		this.dataOrdine = dataOrdine;
+		this.numero = numero;
+		this.cliente = cliente;
+		this.listaArticoli = listaArticoli;
+	}
+	
+	
+	
+	
 }
